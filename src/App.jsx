214bc6bc1938ee373b83f101ccf869223ce70fdd
minseo121 <kittse1217@gmail.com>
2025-09-08 19:07:16 +0900

    [add]프로젝트 기본 세팅

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Start from "./pages/Start";
+
+function App() {
+  return (
+    <>
+      <div className="App  h-full">
+        <Start />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Start.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Start = () => {
+    return (
+        <div className='w-full min-h-screen flex justify-center items-center'>
+            start 화면입니다
+        </div>
+    );
+};
+
+export default Start;
