244e21d8accedc1b76ef3f1adb2ff3dd0515605a
minseo121 <kittse1217@gmail.com>
2025-09-14 02:52:48 +0900

    [add]폰트 설정

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Start = () => {
     return (
         <div className='w-full min-h-screen flex justify-center items-center'>
-            start 화면입니다
+            <p className='UhBee'>start 화면입니다</p>
         </div>
     );
 };
